f0dc474bfd212107d776582be3da769969f2cba8
Christopher T. Herrera <eefretsoul@gmail.com>
2020-08-12 16:12:45 -0400

    adding module 3

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync("src/json/accounts.json", {
   encoding: "utf8",
@@ -34,6 +35,46 @@ app.get("/credit", function (req, res) {
 
 app.get("/profile", function (req, res) {
   res.render("profile", { user: users[0] });
+});
+
+app.get("/transfer", function (req, res) {
+  res.render("transfer");
+});
+
+app.post("/transfer", function (req, res) {
+  var from = req.body.from;
+  var to = req.body.to;
+  var amount = req.body.amount;
+
+  accounts[from].balance -= parseInt(amount);
+  accounts[to].balance += parseInt(amount);
+  var accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "/json/accounts.json"),
+    accountsJSON,
+    "utf-8"
+  );
+  res.render("transfer", { message: "Transfer Completed" });
+});
+
+app.get("/payment", function (req, res) {
+  res.render("payment", { account: accounts.credit });
+});
+
+app.post("/payment", function (req, res) {
+  var amount = req.body.amount;
+  accounts.credit.balance -= parseInt(amount);
+  accounts.credit.available += parseInt(amount);
+  var accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "/json/accounts.json"),
+    accountsJSON,
+    "utf-8"
+  );
+  res.render("payment", {
+    message: "Payment Successful",
+    account: accounts.credit,
+  });
 });
 
 app.listen(3000, function () {
