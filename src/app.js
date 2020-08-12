174d0fdcd2a860bfaf95638f0d3390e5903429e4
Christopher T. Herrera <eefretsoul@gmail.com>
2020-08-12 09:30:44 -0400

    adding module 2

--- src/app.js
+++ src/app.js
@@ -8,8 +8,32 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 
+const accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "utf8",
+});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync("src/json/users.json", { encoding: "utf8" });
+const users = JSON.parse(userData);
+
 app.get("/", function (req, res) {
-  res.render("index", { title: "Index" });
+  res.render("index", { title: "Account Summary", accounts: accounts });
+});
+
+app.get("/savings", function (req, res) {
+  res.render("account", { account: accounts.savings });
+});
+
+app.get("/checking", function (req, res) {
+  res.render("account", { account: accounts.checking });
+});
+
+app.get("/credit", function (req, res) {
+  res.render("account", { account: accounts.credit });
+});
+
+app.get("/profile", function (req, res) {
+  res.render("profile", { user: users[0] });
 });
 
 app.listen(3000, function () {
